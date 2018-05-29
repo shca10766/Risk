86c87bc34459a561656f7f95a6596fe4d9ef8b9c
shca10766 <shirley.caillere@gmail.com>
2018-05-29 12:39:46 +0200

    niaq

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//TAC
+		//TACj
 		String [] tab = {
 				"Alaska", "Territoires du Nord", "Groenland", "Alberta", "Ontario", "Quebec", "Ouest des Etats-Unis", "Est des Etats-Unis", "Amérique Centrale",
 				"Venezuela", "Brésil", "Péru", "Argentine",
